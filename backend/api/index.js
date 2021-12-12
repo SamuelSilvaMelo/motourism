711f7dae6fdbb7cea37c10567153144ccc532807
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-12-12 15:10:38 -0300

    FEAT: Add initial API files

---
+++ backend/api/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`App running on PORT: ${PORT}`));
