100062104beb8c33ac746880bca73b486d437fbe
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-12-13 12:04:19 -0300

    feat: add scrollingIcon

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import background from '../assets/Background.svg';
 import logo from '../assets/Logo.svg';
+import scrollingIcon from '../assets/scrollingIcon.png';
 import '../styles/Main.css';
 
 const Main = () => (
@@ -10,6 +11,7 @@ const Main = () => (
       <button className="Button" type="button">Explorar Roteiros</button>
     </div>
     <img className="Background-img" src={background} alt="imagem de fundo" />
+    <img className="Scrolling-icon" src={scrollingIcon} alt="scrollingIcon" />
   </section>
 );
 
