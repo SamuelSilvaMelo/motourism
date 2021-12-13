465fb10863a2435409944bfc2458f490e4f089dc
Gui <guilhermepdo1508@gmail.com>
2021-12-13 09:36:24 -0300

    FEAT: HTML made from login page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../assets/Logo.png';
+import Logo from '../assets/Logo.svg';
 import MenuButton from '../assets/MenuButton.png';
 
 const Header = () => {
--- frontend/src/pages/User/Login.jsx
+++ frontend/src/pages/User/Login.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import User from '../../assets/User.png';
+import Password from '../../assets/Password.png';
 
 const Login = () => (
-  <h1>Login</h1>
+  <div>
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label htmlFor="email">
+          <img width="30" src={User} alt="User icon" />
+          <input id="email" type="email" placeholder="E-mail" />
+        </label>
+        <label htmlFor="password">
+          <img width="30" src={Password} alt="Password icon" />
+          <input id="password" type="password" placeholder="Senha" />
+        </label>
+        <button type="button">Entrar</button>
+      </form>
+    </div>
+    <div>
+      <h1>Ou criar conta</h1>
+      <form>
+        <label htmlFor="create-email">
+          <img width="30" src={User} alt="User icon" />
+          <input id="create-email" type="email" placeholder="E-mail" />
+        </label>
+        <label htmlFor="create-name">
+          <img width="30" src={User} alt="User icon" />
+          <input id="create-name" type="text" placeholder="Nome" />
+        </label>
+        <label htmlFor="create-password">
+          <img width="30" src={Password} alt="Password icon" />
+          <input id="create-password" type="password" placeholder="Senha" />
+        </label>
+        <label htmlFor="create-confirm-password">
+          <img width="30" src={Password} alt="Password icon" />
+          <input id="create-confirm-password" type="password" placeholder="Confirmar senha" />
+        </label>
+        <button type="button">Entrar</button>
+      </form>
+    </div>
+  </div>
 );
 
 export default Login;
