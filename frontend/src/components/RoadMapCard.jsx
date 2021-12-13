fdf1eb18c89cb568807542f32a59f4ce91694487
Gui <guilhermepdo1508@gmail.com>
2021-12-13 11:36:56 -0300

    STYLE: stylized itinerary page

--- frontend/src/components/RoadMapCard.jsx
+++ frontend/src/components/RoadMapCard.jsx
@@ -4,8 +4,24 @@ import { Link } from 'react-router-dom';
 
 const RoadMapCard = ({ roadMap }) => (
   <Link to={`/roteiros/${roadMap.id}`}>
-    <div style={{ backgroundImage: `url("${roadMap.image}")` }}>
-      <div>
+    <div
+      style={{
+        backgroundImage: `url("${roadMap.image}")`,
+        backgroundSize: 'cover',
+        height: '300px',
+      }}
+      className="rounded-3xl flex items-start justify-center"
+    >
+      <div
+        style={{
+          backgroundColor: '#7A6F9B',
+          width: '50%',
+          color: '#F8F0FB',
+          fontSize: '1.3rem',
+          fontWeight: 'bold',
+        }}
+        className="flex flex-col items-center justify-center mt-8 rounded-3xl"
+      >
         <h3>{roadMap.name}</h3>
         <h3>{`${roadMap.distance} Km`}</h3>
         <h3>{`${roadMap.predictedDays} dias`}</h3>
--- frontend/src/pages/RoadMaps/index.jsx
+++ frontend/src/pages/RoadMaps/index.jsx
@@ -6,10 +6,20 @@ const RoadMaps = () => {
   const { roadMaps } = useContext(MouturismDataContext);
 
   return (
-    <main className="text-center text-font-brown">
-      <h1 className="my-8 text-2xl">Roteiros</h1>
+    <main
+      style={{
+        fontFamily: 'Abhaya Libre, serif',
+        color: '#211A1D',
+        backgroundColor: '#F8F0FB',
+        minHeight: 'calc(100vh - 65px)',
+      }}
+      className="flex flex-col items-center text-center text-font-brown"
+    >
+      <h1 className="text-3xl my-8 text-2xl">Roteiros</h1>
       <div className="mx-4">
-        <p>
+        <p
+          className="text-xl"
+        >
           Aqui você pode encontrar roteiros prontos incríveis ou ainda pesquisar
           destinos para seu roteiro personalizado
         </p>
