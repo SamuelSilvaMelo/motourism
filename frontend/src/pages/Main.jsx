f4e00a34ecca11a4a7ad54bd64a1422114a05bf5
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-12-13 13:43:13 -0300

    feat: add two imgs at second section

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import background from '../assets/Background.svg';
 import logo from '../assets/Logo.svg';
 import scrollingIcon from '../assets/scrollingIcon.png';
+import bg1 from '../assets/bg1.png';
+import bg2 from '../assets/bg2.png';
 import '../styles/Main.css';
 
 const Main = () => (
@@ -16,6 +18,18 @@ const Main = () => (
     </section>
     <section className="Second-section">
       <p>Viva essa experiência</p>
+      <div className="First-img">
+        <img className="Bg1" src={bg1} alt="Bg1" />
+        <div className="First-img-text">
+          <p>Roteiros incríveis e personalizados</p>
+        </div>
+      </div>
+      <div className="Second-img">
+        <img className="Bg2" src={bg2} alt="Bg2" />
+        <div className="Second-img-text">
+          <p>Motorhomes confortáveis</p>
+        </div>
+      </div>
     </section>
   </div>
 );
