827decc92967cbab5858f518b752c24fb1f5e0e6
Gui <guilhermepdo1508@gmail.com>
2021-12-13 22:34:39 -0300

    FEAT: made component for errors advices

--- frontend/src/pages/User/Login.jsx
+++ frontend/src/pages/User/Login.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginForm from '../../components/LoginForm';
 import RegisterForm from '../../components/RegisterForm';
+import ErrorAdvice from '../../components/ErrorAdvice';
 
-const Login = () => (
-  <div
-    style={{
-      fontFamily: 'Abhaya Libre, serif',
-      color: '#211A1D',
-      backgroundColor: '#F8F0FB',
-    }}
-    className="flex flex-col items-center"
-  >
-    <LoginForm />
-    <RegisterForm />
-  </div>
-);
+const Login = () => {
+  const [showErrorAdvice, setShowErrorAdvice] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  return (
+    <>
+      {showErrorAdvice && (
+      <ErrorAdvice
+        message={errorMessage}
+        close={() => setShowErrorAdvice(false)}
+      />
+      )}
+      <div
+        style={{
+          fontFamily: 'Abhaya Libre, serif',
+          color: '#211A1D',
+          backgroundColor: '#F8F0FB',
+        }}
+        className="flex flex-col items-center"
+      >
+        <LoginForm
+          setErrorMessage={setErrorMessage}
+          setShowErrorAdvice={setShowErrorAdvice}
+        />
+        <RegisterForm
+          setErrorMessage={setErrorMessage}
+          setShowErrorAdvice={setShowErrorAdvice}
+        />
+      </div>
+    </>
+  );
+};
 
 export default Login;
